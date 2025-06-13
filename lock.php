3f012e65ec37061ffd0f7b1c281c98cf144585f2
Emirybj <emilyperfect76@gmail.com>
2025-06-13 01:19:06 -0300

    Alterando um erro

--- lock.php
+++ lock.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['usuario']) || !isset($_SESSION['senha'])) {
-    header('location:index.php?codigo=0');
+    header('location:index.php?codigo=0'); //Se o usuário não estiver logado ele é redirecionado para a página inicial.
     exit;
 }
 
